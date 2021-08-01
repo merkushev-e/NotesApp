a941ed1d85a580255d85a4d8c5db59a98ffc29e3
Egor M <merkushev.eg@gmail.com>
2021-08-01 13:30:06 +0300

    Lesson 7 not finished

--- app/src/main/java/ru/gb/notesapp/MainActivity.java
+++ app/src/main/java/ru/gb/notesapp/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+     getSupportFragmentManager().popBackStack();
+     super.onBackPressed();
     }
 }
--- app/src/main/java/ru/gb/notesapp/NotesFragment.java
+++ app/src/main/java/ru/gb/notesapp/NotesFragment.java
@@ -1,5 +1,6 @@
 package ru.gb.notesapp;
 
+import android.app.FragmentManagerNonConfig;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
@@ -20,6 +22,8 @@ import android.widget.TextView;
 public class NotesFragment extends Fragment {
 
     public static final int DEFAULT_INDEX = 0;
+    public static final String NOTES_FRAGMENT = "NotesFragment";
+    public static final String NOTES_CONTENT = "Notes_Content";
     private boolean isLand = false;
 
     public NotesFragment() {
@@ -32,16 +36,23 @@ public class NotesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(NOTES_FRAGMENT);
+        fragmentTransaction.commit();
+
         isLand = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
         if (isLand) {
             showNotesContentLand(DEFAULT_INDEX);
         }
+
         initList(view);
     }
 
@@ -72,10 +83,19 @@ public class NotesFragment extends Fragment {
         }
     }
 
-    private void showNotesContentPort(int finalIndex) {
-        Intent intent = new Intent(getActivity(), NotesContentActivity.class);
-        intent.putExtra(NotesContentFragment.ARG_INDEX, finalIndex);
-        startActivity(intent);
+    private void showNotesContentPort(int index) {
+
+
+        NotesContentFragment fragment = NotesContentFragment.newInstance(index);
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.notes_fragments_container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .addToBackStack(null)
+                .commit();
+//        Intent intent = new Intent(getActivity(), NotesContentActivity.class);
+//        intent.putExtra(NotesContentFragment.ARG_INDEX, finalIndex);
+//        startActivity(intent);
 
     }
 
@@ -85,6 +105,7 @@ public class NotesFragment extends Fragment {
                 .beginTransaction()
                 .replace(R.id.notes_content_fragments, fragment)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .addToBackStack(null)
                 .commit();
     }
 
@@ -97,5 +118,7 @@ public class NotesFragment extends Fragment {
         }
         ((TextView) linearLayout.getChildAt(index)).setBackgroundColor(getResources().getColor(R.color.secondaryLightColor));
     }
+
+
 
 }
